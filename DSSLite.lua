fd1045220ecb110ec8694104dad639b7a9af98eb
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2024-04-04 04:36:57 -0700

    Update DSSLite.lua

--- DSSLite.lua
+++ DSSLite.lua
@@ -1,121 +1,138 @@
 local HttpService = game:GetService("HttpService")
 local DataStoreService = game:GetService("DataStoreService")
 
--- DataStorServiceeLite (DSSLite)
-local function DSSLite()
-	local dsmod = {}
-	local DataStore = nil;
-	local cache = nil;
-	local entryKey = nil
+-- Define shared functions
+local DSSLiteMethods = {}
 
-	function dsmod.CopyTable(t)
-		return HttpService:JSONDecode(HttpService:JSONEncode(t))
-	end
-
-	function dsmod.InitStore(key)
-		print("Initializing store: " .. key)
-		if not DataStore then
-			DataStore = DataStoreService:GetDataStore(key)
-			return true
-		end
-		print("use DSSLite:ReleaseStore() before running DSSLite:InitStore(key) again")
-		return false
-	end
-
-	function dsmod.GetStore()
-		return DataStore;
-	end
-
-	function dsmod.ReleaseStore()
-		if DataStore then
-			DataStore = nil
-			return true
-		end
-		print("There is no DataStore to release")
-		return false;
-	end
-
-	-------------------------------------------------------
-	function dsmod.GetData(key)
-		local success, result = pcall(function()
-			return DataStore:GetAsync(key)
-		end)
-		if not success then
-			warn(result)
-		end
-		return success, result
-	end
-
-	function dsmod:GetCacheCopy()
-		return self.CopyTable(cache);
-	end
-
-	function dsmod.LoadIntoCache(key)
-		print("loading data into read only cache")
-		entryKey = key;
-		local success;
-		success, cache = dsmod.GetData(key)
-		print("did system load cache: " .. tostring(success))
-		return success
-	end
-	
-	function dsmod.SaveData(key, data)
-		local success, result = pcall(function()
-			DataStore:SetAsync(key, data)
-		end)
-		if not success then
-			warn(result)
-		end
-		return success, result 
-	end
-
-	function dsmod.ReleaseCache()
-		cache = nil;
-		entryKey = nil;
-		return true;
-	end
-
-	function dsmod:SaveCache()
-		return self:SaveData(entryKey, cache)
-	end
-
-	function dsmod:SaveAndReleaseCache()
-		self.SaveCache();
-		self.ReleaseCache();
-	end
-
-	function dsmod:SaveCacheAndReleaseFull()
-		self.SaveAndReleaseCache();
-		self.ReleaseStore();
-	end
-	-------------------------------------------------------------
-
-	function dsmod.UpdateCache(newData, ...) -- variadic args are any number of nested keys
-		local function update(t, newData, ...)
-			print("searching for key " .. (...) .. " in: ")
-			print(t)
-			if type(t) == "table" then
-				if t[(...)] then
-					print((...) .. " is found in cache")
-					print(select('#', ...))
-					if select('#', ...) == 0 then
-						print("reached end, updating value for key: " .. ((...)))
-						t[(...)] = newData;
-						return true;
-					else
-						return update(t[(...)], newData, select(2, ...))
-					end
-				else
-					return false;
-				end
-			end
-			return false;
-		end
-		return update(cache, newData, ...)
-	end
-	return dsmod
+function DSSLiteMethods:copyTable(t)
+    return HttpService:JSONDecode(HttpService:JSONEncode(t))
 end
 
-return DSSLite
+function DSSLiteMethods:initStore(key)
+    print("Initializing store: " .. key)
+    if not self.DataStore then
+        self.DataStore = DataStoreService:GetDataStore(key)
+        return true
+    end
+    print("Use DSSLite:ReleaseStore() before running DSSLite:InitStore(key) again")
+    return false
+end
 
+function DSSLiteMethods:releaseStore()
+    if self.DataStore then
+        self.DataStore = nil
+        return true
+    end
+    print("There is no DataStore to release")
+    return false
+end
 
+function DSSLiteMethods:getData(key)
+    local success, result = pcall(function()
+        return self.DataStore:GetAsync(key)
+    end)
+    if not success then
+        warn(result)
+    end
+    return success, result
+end
+
+function DSSLiteMethods:saveData(key, data)
+    local success, result = pcall(function()
+        self.DataStore:SetAsync(key, data)
+    end)
+    if not success then
+        warn(result)
+    end
+    return success, result
+end
+
+function DSSLiteMethods:loadIntoCache(key)
+    print("Loading data into read-only cache")
+    self.entryKey = key
+    local success
+    success, self.cache = self:getData(key)
+    print("Did system load cache: " .. tostring(success))
+    return success
+end
+
+function DSSLiteMethods:releaseCache()
+    self.cache = nil
+    self.entryKey = nil
+    return true
+end
+
+-- Define SavableDSSLiteMethods
+local SavableDSSLiteMethods = {}
+
+function SavableDSSLiteMethods:saveCache()
+    return self:saveData(self.entryKey, self.cache)
+end
+
+function SavableDSSLiteMethods:saveAndReleaseCache()
+    self:saveCache()
+    self:releaseCache()
+end
+
+function SavableDSSLiteMethods:saveCacheAndReleaseFull()
+    self:saveAndReleaseCache()
+    self:releaseStore()
+end
+
+-- METATABLE INHERITOR FOR MULTIPLE INHERITANCE
+local function SearchParents(key, parents)
+    for i = 1, #parents do
+        local found = parents[i][key]
+        if found then
+            return found
+        end
+    end
+end
+
+local function RegisterParents(parents)
+    return {
+        __index = function(self, key)
+            return SearchParents(key, parents)
+        end
+    }
+end
+
+-- Define constructor for DataStoreLite instances
+local function CreateDataStoreLite()
+    local dsInstance = {
+        DataStore = nil,
+        entryKey = nil,
+        cache = nil
+    }
+
+    -- Set metatable to access shared functions and private data
+    local mt = {
+        __index = DSSLiteMethods,
+        __newindex = function(tbl, key, value)
+            if dsInstance[key] ~= nil then
+                error("Cannot directly set '" .. key .. "'")
+            else
+                rawset(tbl, key, value)
+            end
+        end
+    }
+
+    setmetatable(dsInstance, mt)
+
+    return dsInstance
+end
+
+-- Define constructor for SavableDSSLite instances
+local function CreateSavableDataStoreLite()
+    local savableDsInstance = CreateDataStoreLite()
+    local savableMt = RegisterParents({SavableDSSLiteMethods, savableDsInstance})
+
+    setmetatable(savableDsInstance, savableMt)
+
+    return savableDsInstance
+end
+
+return {
+    NewDSSLite = CreateDataStoreLite,
+    NewSavableDSSLite = CreateSavableDataStoreLite
+}
